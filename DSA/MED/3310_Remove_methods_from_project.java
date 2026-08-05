2da7cdca37b0a53a86f846e151e7de2dfa54e019
suchitra-reddy-gudibandi <suchithrareddy95@gmail.com>
2026-08-05 20:43:22 +0530

    Added LeetCode 3310 Solution

---
+++ DSA/MED/3310_Remove_methods_from_project.java
@@ -0,0 +1,46 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] edge : invocations) {
+            graph[edge[0]].add(edge[1]);
+        }
+
+        boolean[] suspicious = new boolean[n];
+        dfs(k, graph, suspicious);
+
+        // If any non-suspicious method invokes a suspicious one,
+        // we cannot remove anything.
+        for (int[] edge : invocations) {
+            if (!suspicious[edge[0]] && suspicious[edge[1]]) {
+                List<Integer> ans = new ArrayList<>();
+                for (int i = 0; i < n; i++) {
+                    ans.add(i);
+                }
+                return ans;
+            }
+        }
+
+        // Return all non-suspicious methods.
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i]) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+
+    private void dfs(int node, List<Integer>[] graph, boolean[] suspicious) {
+        if (suspicious[node]) return;
+
+        suspicious[node] = true;
+        for (int next : graph[node]) {
+            dfs(next, graph, suspicious);
+        }
+    }
+}
