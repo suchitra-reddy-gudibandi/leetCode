cbeafb520af8164a7e7b8cc3f583d84ec5c82158
suchitra-reddy-gudibandi <suchithrareddy95@gmail.com>
2026-08-14 22:28:52 +0530

    Added LeetCode 3090 Solution

---
+++ DSA/Easy/3090_Maximum_length_substring_with_two_occurrences.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maximumLengthSubstring(String s) {
+        int[] count = new int[26];
+
+        int left = 0;
+        int maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            int index = s.charAt(right) - 'a';
+            count[index]++;
+
+            // If any character occurs more than twice,
+            // move left until the window becomes valid.
+            while (count[index] > 2) {
+                count[s.charAt(left) - 'a']--;
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+}
