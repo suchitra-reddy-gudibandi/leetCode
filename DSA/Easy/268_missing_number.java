1fffdabf309b0a28ba074129e5258dbdb3972ae0
suchitra-reddy-gudibandi <suchithrareddy95@gmail.com>
2026-08-18 21:57:35 +0530

    Added 268

---
+++ DSA/Easy/268_missing_number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int ans = n;
+        for (int i = 0; i < n; i++) {
+            ans ^= i;
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+}
