e133f911c2e77596c45f07eedd904cb14f316b8b
suchitra-reddy-gudibandi <suchithrareddy95@gmail.com>
2026-08-25 21:51:39 +0530

    Added 396 solution

---
+++ DSA/MED/396_rotate_function.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxRotateFunction(int[] nums) {
+        int n = nums.length;
+        long sum = 0;
+        long f = 0;
+        
+        // Step 1: Compute total array sum and initial F(0)
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            f += (long) i * nums[i];
+        }
+        
+        long maxF = f;
+        
+        for (int k = 1; k < n; k++) {
+            f = f + sum - (long) n * nums[n - k];
+            if (f > maxF) {
+                maxF = f;
+            }
+        }
+        return (int) maxF;
+    }
+}
