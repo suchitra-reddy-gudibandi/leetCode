ed262b0c212f34f2841f0f2f234511e3f4867005
suchitra-reddy-gudibandi <suchithrareddy95@gmail.com>
2026-08-29 22:19:57 +0530

    Added Leetcode 367 Solution

---
+++ DSA/Easy/367_valid_perfect_square.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long low = 1, high = num;
+
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+            long square = mid * mid;
+
+            if (square == num) {
+                return true;
+            } else if (square < num) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
