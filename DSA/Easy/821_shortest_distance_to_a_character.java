d8b9c07b186e6554f5837183eeae86a686cac27e
suchitra-reddy-gudibandi <suchithrareddy95@gmail.com>
2026-09-04 23:09:46 +0530

    Added LeetCode 821 Solution

---
+++ DSA/Easy/821_shortest_distance_to_a_character.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] shortestToChar(String s, char c) {
+        int n = s.length();
+        int[] answer = new int[n];
+
+        int prev = -n;
+
+        // Left to right
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == c) {
+                prev = i;
+            }
+            answer[i] = i - prev;
+        }
+
+        prev = 2 * n;
+
+        // Right to left
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == c) {
+                prev = i;
+            }
+            answer[i] = Math.min(answer[i], prev - i);
+        }
+
+        return answer;
+    }
+}
