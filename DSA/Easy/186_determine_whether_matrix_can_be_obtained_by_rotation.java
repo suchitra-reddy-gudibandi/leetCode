fb98de4dc43391cf2159ea45f9ebd23e456936bc
suchitra-reddy-gudibandi <suchithrareddy95@gmail.com>
2026-09-06 23:58:41 +0530

    Added 186 Solution

---
+++ DSA/Easy/186_determine_whether_matrix_can_be_obtained_by_rotation.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean findRotation(int[][] mat, int[][] target) {
+        for (int k = 0; k < 4; k++) {
+            if (isEqual(mat, target)) {
+                return true;
+            }
+            rotate(mat);
+        }
+        return false;
+    }
+
+    private boolean isEqual(int[][] mat, int[][] target) {
+        int n = mat.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] != target[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
